75c47faff611e802e06c38aa36dbc92ec74a11be
Manny <emanual4real@hotmail.com>
2026-06-26 16:53:38 +0000

    update chapter 5

--- 05.task_Mad_Lib/Program.cs
+++ 05.task_Mad_Lib/Program.cs
@@ -1,34 +1,14 @@
-﻿using System;
+﻿string? color, pluralNoun, celebrity;
 
-namespace program {
+Console.Write("Enter A color: ");
+color = Console.ReadLine();
 
-class prog
-    {
-        static void Main(string[] args)
-        {
-            string color, pluralNoun, celebrity;
+Console.Write("Enter A pluralNoun: ");
+pluralNoun = Console.ReadLine();
 
-            Console.Write("Enter A color: ");
-            color = Console.ReadLine();
+Console.Write("Enter A celebrity: ");
+celebrity = Console.ReadLine();
 
-            Console.Write("Enter A pluralNoun: ");
-            pluralNoun = Console.ReadLine();
-
-            Console.Write("Enter A celebrity: ");
-            celebrity = Console.ReadLine();
-
-            Console.WriteLine($"Roses are {color}");
-            Console.WriteLine($"{pluralNoun} are blue");
-            Console.WriteLine($"I love {celebrity}");
-            /*      Enter A color: df
-                    Enter A pluralNoun: af
-                    Enter A celebrity: fsa
-                    Roses are df    
-                    af are blue
-                    I love fsa*/
-           
-
-
-        }  
-    }
-}
+Console.WriteLine($"Roses are {color}");
+Console.WriteLine($"{pluralNoun} are blue");
+Console.WriteLine($"I love {celebrity}");
